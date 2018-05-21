cf2b81c9338ba5e1b84ed39624b8e910351dc5b9
Alexandre Toubiana <alextoub@hotmail.com>
2018-05-21 18:23:15 +0200

    [BACK] Refactoring files

--- backend/api/app.js
+++ backend/api/app.js
@@ -4,14 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./index');
+var usersRouter = require('./controllers/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,20 +18,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(500).json({
+    message: err.message,
+    error: err
+  });
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(4242, () => {
+  console.log('Example app listening on port 4242');
 });
 
 module.exports = app;
--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var books = require('./data_access/books');
+var books = require('./controllers/books');
 
 router.get('/api/books', books.getAllBooks);
 
