5b86e8292f7e8d1b3d370b0f28e5b12b8a55588c
Benjamin Budet <benjamin.budet@epita.fr>
2018-05-22 22:56:49 +0200

    [LOGIN] Add simple form with custom keyboard and secure password

--- frontend/Bookr/Components/Login/Login.js
+++ frontend/Bookr/Components/Login/Login.js
@@ -1,10 +1,74 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, AppRegistry, StyleSheet, View, TouchableHighlight } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+
+var t = require('tcomb-form-native');
+
+var Form = t.form.Form;
+
+var User = t.struct({
+  username: t.String,
+  password: t.String,
+  rememberMe: t.Boolean
+});
+
+var options = {
+  order: ['username', 'password'],
+  fields: {
+  username: {
+    placeholder: 'rodrigue@rodrigue.com',
+    error: 'Insert a valid email',
+    keyboardType: 'email-address'
+  },
+  password: {
+    secureTextEntry: 'true'
+    }
+  }
+};
+
 
 export default class Login extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
+      <View style={styles.container}>
+        <Form
+          ref="form"
+          type={User}
+          options={options}
+        />
+        <TouchableHighlight style={styles.button} onPress={() => console.log("press")} underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableHighlight>
+      </View>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: 'transparent'
+  },
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 30
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#000',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: 'transparent',
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+});
