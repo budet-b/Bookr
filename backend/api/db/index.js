f154ab8f89e6a0052e7a47f8635686a668762011
Alexandre Toubiana <alextoub@hotmail.com>
2018-05-22 12:36:22 +0200

    [BACK] Move configuration db into specific file

---
+++ backend/api/db/index.js
@@ -0,0 +1,11 @@
+var promise = require('bluebird');
+
+var options = {
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://localhost:5432/signet';
+var db = pgp(connectionString);
+
+module.exports = db;
