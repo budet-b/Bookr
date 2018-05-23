df542b3fa3cf64038d1eb5035c008802a4651c56
Benjamin Budet <benjamin.budet@epita.fr>
2018-05-23 16:47:11 +0200

    [LOGIN] Implemen form with http call to api

--- frontend/Bookr/Components/Login/Login.js
+++ frontend/Bookr/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, AppRegistry, StyleSheet, View, TouchableHighlight } from 'react-native';
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+import axios from 'axios'
 
 var t = require('tcomb-form-native');
 
@@ -8,8 +9,7 @@ var Form = t.form.Form;
 
 var User = t.struct({
   username: t.String,
-  password: t.String,
-  rememberMe: t.Boolean
+  password: t.String
 });
 
 var options = {
@@ -21,13 +21,59 @@ var options = {
     keyboardType: 'email-address'
   },
   password: {
-    secureTextEntry: 'true'
+    secureTextEntry: true
     }
   }
 };
 
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.shouldConnect = this.shouldConnect.bind(this);
+    this.onPress = this.onPress.bind(this);
+  }
+
+  onPress() {
+    var value = this.refs.form.getValue();
+    if (value) {
+      var user = User({
+        username: value.username,
+        password: value.password
+      })
+      console.log(user); // value here is an instance of Person
+      axios.post("http://localhost:8080/login", {
+        username: user.username,
+        password: user.password
+      })
+      .then((response) => {
+        console.log(response)
+      }).catch((error) => {
+        console.log(error)
+      })
+    } else {
+      //
+    }
+  }
+
+  shouldConnect() {
+    let username = "";
+    console.log("form");
+    console.log(this);
+    console.log(this._form.getValue());
+    let password = "";
+    /*axios.post("http://localhost:8080/api/login/", {
+      username: username,
+      password: password
+    })
+    .then((response) => {
+      console.log(response)
+    }).catch((error) => {
+      console.log(error)
+    })*/
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -35,8 +81,9 @@ export default class Login extends Component {
           ref="form"
           type={User}
           options={options}
+          style={styles.form}
         />
-        <TouchableHighlight style={styles.button} onPress={() => console.log("press")} underlayColor='#99d9f4'>
+      <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableHighlight>
       </View>
