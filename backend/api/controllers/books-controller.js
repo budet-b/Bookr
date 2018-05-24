cc59b18c96bc6c580e8010acc069d4df713d9b5e
Alexandre Toubiana <alextoub@hotmail.com>
2018-05-24 23:36:20 +0200

    [BACK] Books - GET book with id

--- backend/api/controllers/books-controller.js
+++ backend/api/controllers/books-controller.js
@@ -27,6 +27,28 @@ function getAllBooks(req, res, next) {
       });
 }
 
+function getBook(req, res) {
+  db.one('select book.id,\
+                 book.isbn,\
+                 book.title,\
+                 book.number_of_pages,\
+                 book.publish_date,\
+                 book.cover,\
+                 author.id as author_id,\
+                 author.name as author_name\
+          from book\
+          inner join author on author.id = book.author_id\
+          where book.id = $1', [req.params.id])
+  .then(function (data) {
+    res.status(200)
+      .json(data);
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
 module.exports = {
-    getAllBooks: getAllBooks
+    getAllBooks: getAllBooks,
+    getBook: getBook
 };
