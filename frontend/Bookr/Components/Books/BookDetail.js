beb7ed7698d581657f5b72f41abeae365bed9a0f
Benjamin Budet <benjamin.budet@epita.fr>
2018-06-06 01:04:32 +0200

    [BOOK] Add friend list on book detail page

--- frontend/Bookr/Components/Books/BookDetail.js
+++ frontend/Bookr/Components/Books/BookDetail.js
@@ -1,20 +1,72 @@
 import React, { Component } from 'react';
-import { Text, AppRegistry, StyleSheet, View, TouchableHighlight, AsyncStorage, Alert, Platform, Image } from 'react-native';
+import { Text, AppRegistry, StyleSheet, View, TouchableHighlight, AsyncStorage, Alert, Platform, Image, ListView, ScrollView } from 'react-native';
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 import { Route, Redirect } from 'react-router'
 import axios from 'axios'
 import { iOSUIKit } from 'react-native-typography';
 import { Slider } from 'react-native-usit-ui';
 
+class Friend extends Component {
+  render() {
+    return (
+      <View style={styles.friend}>
+        <Image
+          borderRadius={8}
+          source={{uri: this.props.friend.img}}
+          style={styles.thumbnail}
+        />
+          <Text
+          style={styles.item}
+          >{this.props.friend.title}</Text>
+          <Text
+          style={styles.descItem}
+          >Page {this.props.friend.page}</Text>
+      </View>
+    )
+  }
+}
+
 export default class BookDetail extends Component {
   constructor(props) {
     super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       bookId: 0,
       bookName: '',
       bookImg: '',
       bookIsbn: 0,
-      isLoading: true
+      isLoading: true,
+      dataSource: ds.cloneWithRows([{
+        title: 'Jane',
+        img: 'https://via.placeholder.com/200x200',
+        isbn: 1213,
+        page: 30,
+        id: 1
+      }, {
+        title: 'Stanislas',
+        img: 'https://via.placeholder.com/200x200',
+        isbn: 42,
+        page: 50,
+        id: 2
+      }, {
+        title: 'Didier',
+        img: 'https://via.placeholder.com/200x200',
+        isbn: 1213,
+        page: 30,
+        id: 3
+      },  {
+        title: 'Book 2',
+        img: 'https://via.placeholder.com/200x200',
+        isbn: 1213,
+        page: 20,
+        id: 4
+      }, {
+        title: 'Book 3',
+        img: 'https://via.placeholder.com/200x200',
+        isbn: 1213,
+        page: 30,
+        id: 12
+      }])
     }
   }
 
@@ -58,7 +110,7 @@ export default class BookDetail extends Component {
         <Text style={styles.left}>page 100</Text>
         <View style={styles.center}>
         <Slider initialValue={100} min={0} max={200} lineStyle={{backgroundColor: '#007AFF', height: 1}} markerStyle={{ width: 10, height: 10, borderRadius: 10 / 2, borderWidth: 1, color:'#F40A12', borderColor: '#F40A12'}} color='#F40A12'/>
-        <Text style={styles.left}>Suspendisse id odio vehicula, maximus leo sed, placerat dolor. Proin eget fermentum turpis. Morbi magna massa, euismod et tempus non, pulvinar at nisi. Curabitur facilisis faucibus massa. Nullam ut nulla rutrum, pretium massa et, mollis augue. Vivamus vitae interdum justo.</Text>
+        <Text style={styles.left}>Suspendisse id odio vehicula, maximus leo sed, placerat dolor. Proin eget fermentum turpis. Morbi magna massa, euismod et tempus non, massa et, mollis augue. Vivamus vitae interdum justo.</Text>
         </View>
         <View>
         <Text style={styles.head}>Friends</Text>
@@ -68,13 +120,57 @@ export default class BookDetail extends Component {
             borderBottomWidth: 1,
           }}
         />
+        <ScrollView
+        horizontal={true}
+        >
+          <ListView contentContainerStyle={styles.list}
+          dataSource={this.state.dataSource}
+          renderRow={(data) => this.renderItem(data)}
+          />
+          </ScrollView>
         </View>
       </View>
     );
   }
+
+  renderItem(item) {
+      return <Friend friend={item}/>
+  }
 }
 
 var styles = StyleSheet.create({
+  list: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    paddingTop: 3
+  },
+  descItem: {
+    color: 'grey',
+    margin: 1,
+    width: 80,
+    alignSelf: 'flex-end',
+    textAlign: 'center'
+  },
+  item: {
+    margin: 1,
+    width: 80,
+    alignSelf: 'flex-end',
+    textAlign: 'center'
+  },
+  friend: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    paddingLeft: 4,
+    paddingRight: 4,
+    height: 200
+  },
+  thumbnail: {
+    width: 60,
+    height: 60,
+    justifyContent: 'flex-end'
+  },
   left: {
     ...iOSUIKit.footnoteEmphasizedObject,
       marginHorizontal: 0,
