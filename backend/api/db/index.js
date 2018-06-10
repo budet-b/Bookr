8f1799c91bc46ca1e956a0baf26afb34e26c46cf
Alexandre Toubiana <alextoub@hotmail.com>
2018-06-10 23:37:01 +0200

    [BACK] Add ssl to true in pgp connection

--- backend/api/db/index.js
+++ backend/api/db/index.js
@@ -5,8 +5,16 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = process.env.DATABASE_URL
+
+const cn = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+};
+console.log(process.env.DATABASE_URL)
+
+var db = pgp(cn);
+// var connectionString = process.env.DATABASE_URL
 // var connectionString = 'postgres://localhost:5432/bookr';
-var db = pgp(connectionString);
+// var db = pgp(connectionString);
 
 module.exports = db;
