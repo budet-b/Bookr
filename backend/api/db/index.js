06b2458764e99cd95f06d9e4446f837b3c56fe44
Alexandre Toubiana <alextoub@hotmail.com>
2018-06-10 23:15:47 +0200

    [BACK] Change db url for heroku

--- backend/api/db/index.js
+++ backend/api/db/index.js
@@ -5,7 +5,8 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/bookr';
+var connectionString = process.env.DATABASE_URL
+// var connectionString = 'postgres://localhost:5432/bookr';
 var db = pgp(connectionString);
 
 module.exports = db;
