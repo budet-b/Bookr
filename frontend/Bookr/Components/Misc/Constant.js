94d3d65fa4abc027618c669b3f034aec9dfda6dd
Benjamin Budet <benjamin.budet@epita.fr>
2018-06-11 11:05:29 +0200

    [CONFIG] Add custom url for api

--- frontend/Bookr/Components/Misc/Constant.js
+++ frontend/Bookr/Components/Misc/Constant.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-
-export default class Constants extends Component {
-  export async saveKey(value) {
-  try {
-    await AsyncStorage.setItem('token', value);
-    } catch (error) {
-    console.log("Error saving data" + error);
-    }
+const dev = {
+  books: {
+    USERBOOK: "http://localhost:8080/api/user/book/",
+    STARTBOOK: "http://localhost:8080/api/books/",
+    ALLBOOKS: "http://localhost:8080/api/books"
+  },
+  user: {
+    USERFRIENDS: "http://localhost:8080/api/user/friends",
+    FRIENDSRECEIVED: "http://localhost:8080/api/friends/received",
+    USER: "http://localhost:8080/api/users",
+    LOGIN: "http://localhost:8080/api/user/login",
+    SIGNUP: "http://localhost:8080/api/user/signup"
   }
+};
 
-  export function async getKey() {
-  try {
-    const value = await AsyncStorage.getItem('token');
-    return value
-  } catch (error) {
-    console.log("Error retrieving data" + error);
-    return null
-    }
+const prod = {
+  books: {
+    USERBOOK: "https://bookr-api.herokuapp.com/api/user/book/",
+    STARTBOOK: "https://bookr-api.herokuapp.com/api/books/",
+    ALLBOOKS: "https://bookr-api.herokuapp.com/api/books"
+  },
+  user: {
+    USERFRIENDS: "https://bookr-api.herokuapp.com/api/user/friends",
+    FRIENDSRECEIVED: "https://bookr-api.herokuapp.com/api/friends/received",
+    USER: "https://bookr-api.herokuapp.com/api/users",
+    LOGIN: "https://bookr-api.herokuapp.com/api/user/login",
+    SIGNUP: "https://bookr-api.herokuapp.com/api/user/signup"
   }
-}
+};
+
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  ...config
+};
