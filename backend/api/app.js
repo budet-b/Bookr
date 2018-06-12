c598a2a4e7adf24d535c22a0b50f6c69d9962c2c
Alexandre Toubiana <alextoub@hotmail.com>
2018-06-12 22:49:32 +0200

    [BACK] Use expressjs cors lib

--- backend/api/app.js
+++ backend/api/app.js
@@ -10,11 +10,13 @@ var logger        = require('morgan');
 var lodash        = require("lodash");
 var bodyParser    = require("body-parser");
 var index         = require('./index');
+var cors          = require('cors');
 
 // MARK: Configuration app
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,8 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next(createError(404))
 });
 app.use((err, req, res, next) => {
