64a87b42dfa5bfbcdbd077c82d231c94431bf1b3
Benjamin Budet <benjamin.budet@epita.fr>
2018-06-13 01:11:09 +0200

    [TL] Started implementation of Timeline with props and TimeLine component

--- frontend/Bookr/Components/Home/HomeComponent.js
+++ frontend/Bookr/Components/Home/HomeComponent.js
@@ -7,15 +7,49 @@ import BottomTabBar from '../BottomTabBar/BottomTabBar';
 import { iOSUIKit, human, material } from 'react-native-typography';
 import config from '../Misc/Constant'
 
+class TimelineCell extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      picture: this.props.cell.user.picture
+    };
+  }
+
+  onError() {
+    this.setState({
+      picture: "https://via.placeholder.com/200x200"
+    })
+  }
+
+  render() {
+    console.log(this.props.cell.user)
+    return (
+      <View style={{flexDirection: 'row'}}>
+      <Image
+        borderRadius={8}
+        source={{uri: "https://via.placeholder.com/200x200"}}
+        style={styles.thumbnail}
+        onError={this.onError.bind(this)}
+      />
+      <Text>{this.props.cell.book.title}</Text>
+      </View>
+    )
+  }
+}
+
+
 export default class HomeComponent extends Component {
   constructor(props) {
   super(props);
+  const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
   this.state = {
-      isLoading: false,
+      isLoading: true,
       userBooks: [],
       pureFriendArray: [],
+      timeline: [],
+      timelineDataSource: ds
     }
-  this.friendPage = this.friendPage.bind(this);
+    this.friendPage = this.friendPage.bind(this);
   }
 
     async getKey() {
@@ -41,7 +75,6 @@ export default class HomeComponent extends Component {
     }
 
     friendPage() {
-      console.log('friend page')
       this.props.screenProps.rootNavigation.navigate('FriendComponent', {screenProps: this.props.screenProps.rootNavigation})
     }
 
@@ -66,10 +99,17 @@ export default class HomeComponent extends Component {
         console.log(error)
       })
 
-
-
-      this.setState({
-        isLoading: false
+      axios.get(config.user.TIMELINE, header)
+      .then((response) => {
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          timeline: response.data,
+          timelineDataSource: ds.cloneWithRows(response.data),
+          isLoading: false
+        })
+      })
+      .catch((error) => {
+        console.log(error)
       })
     }
 
@@ -78,6 +118,50 @@ export default class HomeComponent extends Component {
         await AsyncStorage.setItem('token', value);
       } catch (error) {
         console.log("Error saving data" + error);
+      }
+    }
+
+    ListViewItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: .5,
+          width: "100%",
+          backgroundColor: "#000",
+        }}
+        />
+      );
+    }
+
+    renderTimelineItem(item) {
+      let picture = item.picture ? item.picture : "https://via.placeholder.com/200x200"
+      item.picture = picture
+      console.log(item.picture)
+      return <TimelineCell cell={item} screenProps={{ rootNavigation: this.props.screenProps.rootNavigation}}/>
+    }
+
+    renderTimeline() {
+       if (this.state.timeline.length === 0) {
+        return (
+          <View style={{flex: 1, paddingTop: 20}}>
+          <Text>
+            Sorry, please add some friend to have activity.
+          </Text>
+          </View>
+        )
+      } else {
+        console.log(this.state.timeline)
+        return (
+          <View style={styles.MainContainer}>
+            <ListView
+              dataSource={this.state.timelineDataSource}
+              renderSeparator= {this.ListViewItemSeparator}
+              renderRow={(rowData) => this.renderTimelineItem(rowData)}
+              enableEmptySections={true}
+              style={{marginTop: 10}}
+            />
+          </View>
+        )
       }
     }
 
@@ -120,20 +204,38 @@ export default class HomeComponent extends Component {
         </View>
       )
     });
+    let timeline = this.renderTimeline()
     return (
-      <View>
-      <Text> Didier 2 </Text>
+      <View style={{flex: 1, paddingTop: 20}}>
+      {timeline}
       </View>
     );
   }
 }
 
 var styles = StyleSheet.create({
+  MainContainer :{
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    flex:1,
+    paddingTop: 5
+  },
   rightHead: {
     flex: 1,
     flexDirection: "row",
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  thumbnail: {
+    borderWidth:1,
+    borderColor:'rgba(0,0,0,0.2)',
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'#fff',
+    borderRadius:32,
+    width: 64,
+    height: 64,
+    justifyContent: 'flex-end'
   },
   badgeStyle:{
     textAlign: 'right',
